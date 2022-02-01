7cebe953b1e9bb85136154c591d13c9701d4df81
mosserryan <mosser.ryan.a@gmail.com>
2022-02-01 12:08:09 -0800

    Working on the java writer feature.

---
+++ src/FileClass/FileWriterClass.java
@@ -0,0 +1,23 @@
+//package FileClass;
+//
+//import java.io.FileWriter;
+//import java.io.IOException;
+//
+//public class FileWriterClass {
+//
+//    FileWriterClass () {
+//        writeDocument();
+//    }
+//
+//    public static void writeDocument() {
+//        try {
+//            FileWriter writer = new FileWriter("TextDoc.txt");
+//            writer.write("Melvins be trippin.");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//
+//
+//}
--- src/FileClass/Main.java
+++ src/FileClass/Main.java
@@ -1,12 +1,14 @@
 package FileClass;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
 
     public static void main (String[] args) {
 
-        File file = new File("C:\\Program Files (x86)\\Steam\\steamapps\\common\\SkyrimVR");
+        File file = new File("TextDoc.txt");
 
         if(file.exists()) {
             System.out.println("The file exists!");
@@ -17,6 +19,16 @@ public class Main {
             System.out.println("That file doesn't exist");
         }
 
+        try {
+            FileWriter writer = new FileWriter("TextDoc.txt");
+            writer.write("Melvins be trippin.");
+            writer.close(); // Must close to make changes.
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
+
 
 }
