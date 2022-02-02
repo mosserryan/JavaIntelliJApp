da3f87be4d97c631cd5547cc7f7ea46b32be13e3
mosserryan <mosser.ryan.a@gmail.com>
2022-02-02 15:50:30 -0800

    File reader implemented.

--- src/FileClass/Main.java
+++ src/FileClass/Main.java
@@ -1,6 +1,7 @@
 package FileClass;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -27,6 +28,17 @@ public class Main {
             e.printStackTrace();
         }
 
+        try {
+            FileReader reader = new FileReader("TextDoc.txt");
+            int data = reader.read();
+            while(data != -1) {
+                System.out.print((char)data);
+                data = reader.read();
+            }
+            reader.close();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
