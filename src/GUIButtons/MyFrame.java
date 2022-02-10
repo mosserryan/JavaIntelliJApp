b77d2ef4c80adca638d0d58d99ceaf703de7eff8
mosserryan <mosser.ryan.a@gmail.com>
2022-02-10 14:40:31 -0800

    More progress bar and button interactions. Created pixel art to include three different options and a working health bar.

--- src/GUIButtons/MyFrame.java
+++ src/GUIButtons/MyFrame.java
@@ -7,7 +7,9 @@ import java.awt.event.ActionListener;
 
 public class MyFrame extends JFrame implements ActionListener {
 
-    JButton button;
+    JButton attackButton;
+    JButton healButton;
+    JButton shieldButton;
 
     JProgressBar bar = new JProgressBar();
 
@@ -16,44 +18,78 @@ public class MyFrame extends JFrame implements ActionListener {
 //        ImageIcon swordIcon = new ImageIcon("sword.png");
 //        swordIcon.setIcon();
 
-        ImageIcon imageIcon = new ImageIcon(new ImageIcon("sword.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT));
+        ImageIcon swordIcon = new ImageIcon("sword.png");
+        swordIcon.setImage(swordIcon.getImage().getScaledInstance(125, 125, Image.SCALE_DEFAULT));
+
+        ImageIcon healingIcon = new ImageIcon("redPotion.png");
+        healingIcon.setImage(healingIcon.getImage().getScaledInstance(125, 125, Image.SCALE_DEFAULT));
+
+        ImageIcon shieldIcon = new ImageIcon("shield.png");
+        shieldIcon.setImage(shieldIcon.getImage().getScaledInstance(125, 125, Image.SCALE_DEFAULT));
+
         //label.setIcon(imageIcon);
 
-        button = new JButton();
-        button.setBounds(200,200, 200, 130);
-        button.addActionListener(this);
-        button.setText("Attack");
-        button.setFocusable(false);
-        button.setIcon(imageIcon);
-        button.setHorizontalTextPosition(JButton.CENTER);
-        button.setVerticalTextPosition(JButton.BOTTOM);
-        button.setFont(new Font("Comic Sans", Font.BOLD, 25));
-        button.setIconTextGap(25);
-        button.setForeground(Color.DARK_GRAY);
-        bar.setValue(50);
+        attackButton = new JButton();
+        attackButton.setBounds(400,200, 200, 150);
+        attackButton.addActionListener(this);
+        attackButton.setFocusable(false);
+        attackButton.setIcon(swordIcon);
+
+        //attackButton.setForeground(Color.DARK_GRAY);
+        //attackButton.setText("Attack");
+        //attackButton.setHorizontalTextPosition(JButton.CENTER);
+        //attackButton.setVerticalTextPosition(JButton.BOTTOM);
+        //attackButton.setFont(new Font("Comic Sans", Font.BOLD, 30));
+        //attackButton.setIconTextGap(0);
+
+        healButton = new JButton();
+        healButton.setBounds(700,200, 200, 150);
+        healButton.addActionListener(this);
+        healButton.setFocusable(false);
+        healButton.setIcon(healingIcon);
+
+        shieldButton = new JButton();
+        shieldButton.setBounds(100,200, 200, 150);
+        shieldButton.addActionListener(this);
+        shieldButton.setFocusable(false);
+        shieldButton.setIcon(shieldIcon);
+
+
+        bar.setValue(100);
         bar.setForeground(Color.red);
         bar.setBounds(100,50,400,50);
+        bar.setStringPainted(true);
+        bar.setString("100/100");
+        bar.setForeground(Color.red);
+        bar.setBackground(Color.black);
         //button.setBackground(Color.RED);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
-        this.setSize(1000,700);
+        this.setSize(1000,1000);
         this.setVisible(true);
-        this.add(button);
+        this.add(attackButton);
         this.add(bar);
+        this.add(healButton);
+        this.add(shieldButton);
+
+
 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        if(e.getSource() == button) {
-            System.out.println("I've been pressed!");
+        if(e.getSource() == attackButton) {
+            System.out.println("You attack your target and deal 10 damage!");
             this.setSize(700,700);
 
             if(bar.getValue() > 0) {
                 bar.setValue(bar.getValue() - 10);
-            } else {
-                button.setEnabled(false);
+                bar.setString(bar.getValue() + "/100");
+            }
+
+            if(bar.getValue() < 1) {
+                attackButton.setEnabled(false);
             }
 
         }
